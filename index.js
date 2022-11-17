777413db71dbac3ccbf9f4f29030006c5973b0ed
AnastasiiaBeltsova <102054895+AnastasiiaBeltsova@users.noreply.github.com>
2022-11-17 18:27:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,40 +4,48 @@ function GetId() {
 
     btn.addEventListener('click', function () {
         fetch('https://jsonplaceholder.typicode.com/posts')
-            
-                .then(response => response.json())
-                .then(response => {
+            .then(response => response.json())
+            .then(response => {
+                const idFromInput = input.value;
+                if (idFromInput > 100) {
+                    alert('Ошибка! Введите значение от 1 до 100')
+                }
+                const foundById = response && response.find(u => +u.id === +idFromInput);
+
+                if (foundById) {
                     document.body.insertAdjacentHTML('beforeend', `
-                    <div>${response.map(n => `
+                    <div>${[foundById].map(n => `
                         <div class="item">
                         <div>UserId: ${n.userId}</div>
                         <div>Id: ${n.id}</div>
                         <div>Title: ${n.title}</div>
                         <div>Body: ${n.body}</div>
                         </div>`).join('')}
-                    </div> `);
+                    </div>`);
 
-                    // console.log(response);
-                });
-                // if (input.value <= 100) {
+                    fetch('https://jsonplaceholder.typicode.com/comments')
+                        .then(response => response.json())
+                        .then(response => {
+                            const foundComment = response && response.filter(r => +r.postId === +foundById.id);
+                            if (foundComment) {
+                                document.body.insertAdjacentHTML('afterend', `
+                                <p>____________</p>
+                                <div>${foundComment.map(n => `
+                                    <div class="item">
+                                    <div>PostId: ${n.postId}</div>
+                                    <div>Id: ${n.id}</div>
+                                    <div>Name: ${n.name}</div>
+                                    <div>Email: ${n.email}</div>
+                                    <div>Body: ${n.body}</div>
+                                    </div>`).join('')}
+                                </div>`);
+                            }
+                        })
+                }
 
-                //     console.log(input.value);
-                // }
-        
-            })
-    
+            });
+    })
 }
 
 const getId = new GetId();
 
-
-
-// const input = document.querySelector('.js--input');
-// const btn = document.querySelector('.js--submit');
-// const block = document.querySelector('.js--block');
-
-// const getId = (post) => {
-//     const numb = post.find((id) => {
-//         return id.cc === input.value
-//     })
-// }
